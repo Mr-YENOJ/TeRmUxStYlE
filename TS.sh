0b5574bf0bf04578809d8130419d13b9f4ab480f
Inderveerjoney <62381493+Inderveerjoney@users.noreply.github.com>
2020-04-12 15:28:28 +0530

    Update TS.sh

--- TS.sh
+++ TS.sh
@@ -5,8 +5,6 @@ echo " "
 echo " "
 pkg install toilet -y
 echo " "
-pkg install figlet -y
-echo " "
 clear
 sleep 2.0
 echo " "
@@ -15,10 +13,16 @@ echo "
 ╚══██╔══╝       ██╔════╝╚══██╔══╝╚██╗ ██╔╝██║     ██╔════╝
    ██║          ███████╗   ██║    ╚████╔╝ ██║     █████╗  
    ██║          ╚════██║   ██║     ╚██╔╝  ██║     ██╔══╝  
-   ██║██╗██╗    ███████║   ██║      ██║   ███████╗███████╗
+   ██║██╗██╗    ███████║   ██║      ██║   ███████╗███████╗ v1.5
    ╚═╝╚═╝╚═╝    ╚══════╝   ╚═╝      ╚═╝   ╚══════╝╚══════╝
                                                        "
 echo " "
+nano name.cow
+echo " "
+sleep 30.0
+echo ""
+pkg install figlet -y
+echo ""
 cp -rf name.cow /data/data/com.termux/files/usr/share/cows
 echo " it dose not take much time wait"
 echo " "
@@ -34,5 +38,7 @@ cd TeRmUxStYlE
 cp -rf bash.bashrc /data/data/com.termux/files/usr/etc
 bash main.sh
 clear
-bash
+nano data/data/com.termux/files/usr/etc/bash.bashrc
+clear
+
 
